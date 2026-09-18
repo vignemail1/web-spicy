ccd97f1caccc3a6aa2e2c32dfbd12f05b46c2ad5
vignemail1 <vignemail1@gmail.com>
2026-09-18 23:44:25 +0200

    Update setup image URL in data.js

--- js/data.js
+++ js/data.js
@@ -32,7 +32,7 @@ const SITE_DATA = {
 
   setup: {
     subtitle: "Le setup d'une vraie snipeuse 🎮",
-    image: "assets/images/games/new-setup.jpg",
+    image: "https://spicy-assets.vignemail1.eu/spicy-setup.png",
     features: [
       { title: "Écran", text: "Gaming monitor haute performance" },
       { title: "Performance", text: "Setup optimisé pour le stream" },
